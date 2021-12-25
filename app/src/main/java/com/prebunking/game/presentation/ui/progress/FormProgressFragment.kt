ea48863b250b47e37af97e37fbf8c20549fd1f79
Ahmad Hamwi <ahmadhamwi999@gmail.com>
2021-12-25 12:57:30 +0200

    Added the fifth screen design

---
+++ app/src/main/java/com/prebunking/game/presentation/ui/progress/FormProgressFragment.kt
@@ -0,0 +1,13 @@
+package com.prebunking.game.presentation.ui.progress
+
+import com.prebunking.game.R
+import com.prebunking.game.databinding.FragmentChoiceBinding
+import com.prebunking.game.presentation.ui.base.BaseFragment
+
+class FormProgressFragment : BaseFragment<FragmentChoiceBinding>() {
+    override fun getLayoutId(): Int = R.layout.fragment_form_progress
+
+    override fun bindLayoutBindings(binding: FragmentChoiceBinding) {
+
+    }
+}
---
+++ app/src/main/java/com/prebunking/game/presentation/ui/progress/choice/ChoiceFragment.kt
@@ -0,0 +1,9 @@
+package com.prebunking.game.presentation.ui.progress.choice
+
+import com.prebunking.game.R
+import com.prebunking.game.databinding.FragmentChoiceBinding
+import com.prebunking.game.presentation.ui.base.BaseFragment
+
+class ChoiceFragment : BaseFragment<FragmentChoiceBinding>() {
+    override fun getLayoutId(): Int = R.layout.fragment_choice
+}
