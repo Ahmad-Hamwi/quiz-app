a53c9d9b12106265959d8e0a1fcf5668290658b4
Ahmad Hamwi <ahmadhamwi999@gmail.com>
2021-12-27 15:19:18 +0200

    Added zigzag badges list. Added finish fragment design.

---
+++ app/src/main/java/com/prebunking/game/presentation/ui/finish/FinishActivity.kt
@@ -0,0 +1,9 @@
+package com.prebunking.game.presentation.ui.finish
+
+import com.prebunking.game.R
+import com.prebunking.game.databinding.FragmentFinishBinding
+import com.prebunking.game.presentation.ui.base.BaseActivity
+
+class FinishActivity : BaseActivity<FragmentFinishBinding>() {
+    override fun getLayoutId(): Int = R.layout.fragment_finish
+}
