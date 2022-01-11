1daad1b9eccd2d34b85937b692f6a976c11c0af2
Ahmad Hamwi <ahmadhamwi999@gmail.com>
2022-01-11 16:29:45 +0200

    Removed Coil functions

--- app/src/main/java/com/prebunking/game/presentation/util/Extensions.kt
+++ app/src/main/java/com/prebunking/game/presentation/util/Extensions.kt
@@ -1,33 +1,6 @@
 package com.prebunking.game.presentation.util
 
-import android.widget.ImageView
 import androidx.lifecycle.MutableLiveData
-import coil.ImageLoader
-import coil.decode.SvgDecoder
-import coil.request.ImageRequest
-import coil.util.CoilUtils
-import okhttp3.OkHttpClient
-
-fun ImageView.loadUrl(url: String?) {
-
-    val imageLoader = ImageLoader.Builder(this.context)
-        .okHttpClient {
-            OkHttpClient.Builder()
-                .cache(CoilUtils.createDefaultCache(context))
-                .build()
-        }
-        .componentRegistry { add(SvgDecoder(this@loadUrl.context)) }
-        .build()
-
-    val request = ImageRequest.Builder(this.context)
-        .crossfade(true)
-        .crossfade(500)
-        .data(url)
-        .target(this)
-        .build()
-
-    imageLoader.enqueue(request)
-}
 
 fun <T> MutableLiveData<MutableList<T>>.add(item: T) {
     val updatedItems = this.value
